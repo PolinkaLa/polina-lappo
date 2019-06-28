acec56bdc8e84eb6ec1c60b8a3f8235e0687e4b8
plappo <polina.lappo@soft-werke.com>
2019-06-28 19:24:58 +0300

    add changing content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+const navUl = document.getElementById('navUl');
 
+navUl.addEventListener("click", (event) => {
+    const toShowSectionId = event.target.id.slice(0, -4);
+    const toShowSection = document.getElementById(toShowSectionId);
+    let activeSection = document.getElementsByClassName("activeSection")[0];
+    activeSection.classList.remove("activeSection");
+    activeSection.classList.add("dont-show");
+    toShowSection.classList.remove("dont-show");
+    toShowSection.classList.add("activeSection")
+    let activeLink = document.getElementsByClassName("active-link")[0];
+    activeLink.classList.remove("active-link");
+    activeLink = document.getElementById(event.target.id);
+    activeLink.classList.add("active-link");
+})
