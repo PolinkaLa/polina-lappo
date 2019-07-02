4dfe21a83dc5e0a9b75dbdac890554ec35ed1dad
Polina Lappo <polina.lappo@soft-werke.com>
2019-07-02 14:32:39 +0300

    style for form and popover

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-const navUl = document.getElementById('navUl');
+const navUl = document.getElementById("navUl");
+const closePopover = document.getElementById("close-popover");
+const popover = document.getElementById("popover");
+
 
 navUl.addEventListener("click", (event) => {
     const toShowSectionId = event.target.id.slice(0, -4);
@@ -12,4 +15,9 @@ navUl.addEventListener("click", (event) => {
     activeLink.classList.remove("active-link");
     activeLink = document.getElementById(event.target.id);
     activeLink.classList.add("active-link");
+})
+
+closePopover.addEventListener("click", (event) => {
+    console.log(popover.classList);
+    popover.classList.add("dont-show");
 })
