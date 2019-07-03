b3baad3cf28179a12eed90e1909e59abe23f7bb5
Polina Lappo <polina.lappo@soft-werke.com>
2019-07-03 16:59:44 +0300

    mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const navUl = document.getElementById("navUl");
 const closePopover = document.getElementById("close-popover");
 const popover = document.getElementById("popover");
+const mobNav = document.getElementById("mob-nav");
+const mobNavClose = document.getElementById("mob-nav-close");
+const nav = document.getElementsByTagName("header")[0];
 
 
 navUl.addEventListener("click", (event) => {
@@ -20,4 +23,25 @@ navUl.addEventListener("click", (event) => {
 closePopover.addEventListener("click", (event) => {
     console.log(popover.classList);
     popover.classList.add("dont-show");
+})
+
+mobNav.addEventListener("touchend", () => {
+    nav.classList.add("mob-nav")
+    mobNav.classList.add("dont-show");
+    mobNavClose.style = "display: block; z-index: 1;";
+})
+
+mobNavClose.addEventListener("touchend", () => {
+    nav.classList.remove("mob-nav");
+    mobNavClose.style = "";
+    mobNav.classList.remove("dont-show");
+})
+
+nav.addEventListener("touchend", () => {
+    setTimeout(() => {
+        nav.classList.remove("mob-nav");
+        mobNavClose.style = "";
+        mobNav.classList.remove("dont-show");
+    }, 700)
+    
 })
