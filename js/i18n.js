e115eee641b9038500732b34c1cc360fa2275978
Polina Lappo <polina.lappo@soft-werke.com>
2019-07-20 01:22:11 +0300

    add link to work from info text

--- js/i18n.js
+++ js/i18n.js
@@ -38,10 +38,10 @@ const polinaJson = {
         },
         "info": {
             "en": "<p>Hi! I`m Polina!</p>"+
-            "<p>I`m developing sites, landing pages and different web-applications. You can see my projects in section <i>WORKS</i>.</p>"+
+            "<p>I`m developing sites, landing pages and different web-applications. You can see my projects in section <span class='link' id='spanlink'>Work</span>.</p>"+
             "<p>If you have a cool idea of the site or application and You want to implement it, write to me on social networks or fill out the form on the site!</p>",
             "ru": "<p>Привет, я Полина!</p>"+
-            "<p>Я занимаюсь разработкой сайтов, посадочных страниц и различных web-приложений. Вы можете посмотеть примеры в разделе <i>РАБОТЫ</i>.</p>"+
+            "<p>Я занимаюсь разработкой сайтов, посадочных страниц и различных web-приложений. Вы можете посмотеть примеры в разделе <span class='link' id='spanlink'>Работы</span>.</p>"+
             "<p>Если у вас есть крутая идея сайта или приложения и Вы хотите реализовать ее, пишите мне в социальных сетях или заполните формуна сайте! </p>"
         },
         "btn":{
@@ -54,8 +54,8 @@ const polinaJson = {
                 "ru": "Скачать резюме"
             },
             "submit": {
-                "en": "Send request",
-                "ru": "Отправить запрос"
+                "en": "Send",
+                "ru": "Отправить"
             },
             "resume":{
                 "en": "resource/resume_lappo_en.pdf",
